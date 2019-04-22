0abf9c70e18824893546a36a96de9056ca75f1df
mathewjkavalam <35956928+mathewjkavalam@users.noreply.github.com>
2019-04-22 11:46:46 +0530

    Create mycode.js

---
+++ mycode.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    if (this.state.count > 10) {
+      return 'You liked this.';
+    }
+
+    return e(
+          'button',
+      { onClick: () => this.setState({ count: this.state.count +1 }) },
+      this.state.count
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+const domContainer2 = document.querySelector('#root2');
+
+ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(LikeButton), domContainer2);
